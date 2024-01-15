051acf5dfba0f6f7f838225beb27593b824d7250
ArinaKo <arinakovalenco7@gmail.com>
2024-01-16 00:10:51 +0300

    Внедрит BlogUserRepository в качестве зависимости

--- project/apps/users/src/app/authentication/authentication.module.ts
+++ project/apps/users/src/app/authentication/authentication.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthenticationService } from './authentication.service';
 import { AuthenticationController } from './authentication.controller';
+import { BlogUserModule } from '../blog-user/blog-user.module';
 
 @Module({
   providers: [AuthenticationService],
   controllers: [AuthenticationController],
+  imports: [BlogUserModule],
 })
 export class AuthenticationModule {}
