604b52195a3e74b17e4b676663225c67356842e0
ArinaKo <arinakovalenco7@gmail.com>
2024-01-16 00:41:56 +0300

    Добавит метод регистрации в AuthenticationService

--- project/apps/users/src/app/authentication/authentication.service.ts
+++ project/apps/users/src/app/authentication/authentication.service.ts
@@ -1,7 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { BlogUserRepository } from '../blog-user/blog-user.repository';
+import { CreateUserDTO } from './dto/create-user.dto';
+import { AUTH_USER_EXISTS } from './authentication.const';
+import { BlogUserEntity } from '../blog-user/blog-user.entity';
 
 @Injectable()
 export class AuthenticationService {
   constructor(private readonly blogUserRepository: BlogUserRepository) {}
+
+  public async register(dto: CreateUserDTO) {
+    const { email, name, password, avatarUrl } = dto;
+
+    const existedUser = await this.blogUserRepository.findByEmail(email);
+
+    if (existedUser) {
+      throw new ConflictException(AUTH_USER_EXISTS);
+    }
+
+    const userEntity = await new BlogUserEntity({
+      email,
+      name,
+      avatarUrl: avatarUrl ?? '',
+      passwordHash: '',
+    }).setPassword(password);
+
+    return this.blogUserRepository.save(userEntity);
+  }
 }
