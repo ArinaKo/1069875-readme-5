b321f1eed53667f30f45c28d7f3056112a76d100
ArinaKo <arinakovalenco7@gmail.com>
2024-01-16 19:13:48 +0300

    Детализирует ответы обработчиков маршрутов (AuthenticationController)

--- project/apps/users/src/app/authentication/rdo/logged-user.rdo.ts
+++ project/apps/users/src/app/authentication/rdo/logged-user.rdo.ts
@@ -1,12 +1,25 @@
 import { Expose } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class LoggedUserRdo {
+  @ApiProperty({
+    description: 'The uniq user ID',
+    example: '111',
+  })
   @Expose()
   public id: string;
 
+  @ApiProperty({
+    description: 'User email',
+    example: 'user@user.ru',
+  })
   @Expose()
   public email: string;
 
+  @ApiProperty({
+    description: 'User access token',
+    example: 'dgj872',
+  })
   @Expose()
   public accessToken: string;
 }
--- project/apps/users/src/app/authentication/rdo/user.rdo.ts
+++ project/apps/users/src/app/authentication/rdo/user.rdo.ts
@@ -1,15 +1,32 @@
 import { Expose } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class UserRdo {
+  @ApiProperty({
+    description: 'The uniq user ID',
+    example: '111',
+  })
   @Expose()
   public id: string;
 
+  @ApiProperty({
+    description: 'User email',
+    example: 'user@user.ru',
+  })
   @Expose()
   public email: string;
 
+  @ApiProperty({
+    description: 'User name',
+    example: 'John',
+  })
   @Expose()
   public name: string;
 
+  @ApiProperty({
+    description: 'User avatar url',
+    example: 'image.jpg',
+  })
   @Expose()
   public avatarUrl: string;
 }
