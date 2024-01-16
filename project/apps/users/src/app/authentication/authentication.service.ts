6652a7e4ec63e2495b1fc2aec48eb2115048ce2f
ArinaKo <arinakovalenco7@gmail.com>
2024-01-16 16:23:57 +0300

    Дополнит методы AuthenticationService

--- project/apps/users/src/app/authentication/authentication.service.ts
+++ project/apps/users/src/app/authentication/authentication.service.ts
@@ -1,8 +1,13 @@
 import { ConflictException, Injectable } from '@nestjs/common';
 import { BlogUserRepository } from '../blog-user/blog-user.repository';
 import { CreateUserDTO } from './dto/create-user.dto';
-import { AUTH_USER_EXISTS } from './authentication.const';
+import {
+  AUTH_USER_EXISTS,
+  AUTH_USER_NOT_FOUND,
+  AUTH_USER_PASSWORD_WRONG,
+} from './authentication.const';
 import { BlogUserEntity } from '../blog-user/blog-user.entity';
+import { LoginUserDTO } from './dto/login-user.dto';
 
 @Injectable()
 export class AuthenticationService {
@@ -25,5 +30,25 @@ export class AuthenticationService {
     }).setPassword(password);
 
     return this.blogUserRepository.save(userEntity);
+  }
+
+  public async verifyUser(dto: LoginUserDTO) {
+    const { email, password } = dto;
+
+    const existedUser = await this.blogUserRepository.findByEmail(email);
+
+    if (!existedUser) {
+      throw new ConflictException(AUTH_USER_NOT_FOUND);
+    }
+
+    if (!(await existedUser.comparePassword(password))) {
+      throw new ConflictException(AUTH_USER_PASSWORD_WRONG);
+    }
+
+    return existedUser;
+  }
+
+  public async getUser(id: string) {
+    return this.blogUserRepository.findById(id);
   }
 }
