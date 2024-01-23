6fc4bbed7653606c90c2c7533afa963d805b368a
ArinaKo <arinakovalenco7@gmail.com>
2024-01-23 16:26:06 +0300

    Подключит MongooseModule в корневой модуль сервиса users

--- project/apps/users/src/app/app.module.ts
+++ project/apps/users/src/app/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
-import { ConfigUsersModule } from '@project/shared/config/users';
+import {
+  ConfigUsersModule,
+  getMongooseOptions,
+} from '@project/shared/config/users';
 import { BlogUserModule } from './blog-user/blog-user.module';
 import { AuthenticationModule } from './authentication/authentication.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ConfigUsersModule, BlogUserModule, AuthenticationModule],
+  imports: [
+    ConfigUsersModule,
+    BlogUserModule,
+    AuthenticationModule,
+    MongooseModule.forRootAsync(getMongooseOptions()),
+  ],
   controllers: [],
   providers: [],
 })
