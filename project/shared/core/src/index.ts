253055201994b69035778db41af94cfca7e9c41a
ArinaKo <arinakovalenco7@gmail.com>
2024-01-28 16:00:31 +0300

    Добавит базовый репозиторий для работы с PostgreSQL

--- project/shared/core/src/index.ts
+++ project/shared/core/src/index.ts
@@ -1,4 +1,7 @@
+import { from } from 'rxjs';
+
 export * from './lib/repository/entity.interface';
 export * from './lib/repository/repository.interface';
 export * from './lib/repository/base-memory.repository';
 export * from './lib/repository/base-mongo.repository';
+export * from './lib/repository/base-postgres.repository';
