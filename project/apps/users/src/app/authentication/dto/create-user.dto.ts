40d59bafe598ad44557fd482046f0dd3388483a6
ArinaKo <arinakovalenco7@gmail.com>
2024-02-01 18:22:02 +0300

    Добавит валидацию для DTO сервиса users

--- project/apps/users/src/app/authentication/dto/create-user.dto.ts
+++ project/apps/users/src/app/authentication/dto/create-user.dto.ts
@@ -1,22 +1,27 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsEmail, IsString } from 'class-validator';
+import { AUTH_USER_EMAIL_NOT_VALID } from '../authentication.const';
 
 export class CreateUserDto {
   @ApiProperty({
     description: 'User email',
     example: 'user@user.ru',
   })
+  @IsEmail({}, { message: AUTH_USER_EMAIL_NOT_VALID })
   public email: string;
 
   @ApiProperty({
     description: 'User name',
     example: 'John',
   })
+  @IsString()
   public name: string;
 
   @ApiProperty({
     description: 'User password',
     example: '123456',
   })
+  @IsString()
   public password: string;
 
   @ApiPropertyOptional({
